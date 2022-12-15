d8107684608aac445b1e96b666e68191eef0504b
Erywelton Andrade <erywelton.andrade@gcinet.com.br>
2022-12-15 16:06:30 -0300

    Commit Parcial- Criação de Controller para funcionalidades do CRUD

---
+++ AlphaFarma/AlphaFarma/Controllers/ProdutosController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlphaFarma.Controllers
+{
+    class ProdutosController
+    {
+        public void Adicionar()
+        {
+
+        }
+
+        public void Excluir()
+        {
+
+        }
+
+        public void Editar()
+        {
+
+        }
+        public void Carregar()
+        {
+
+        }
+    }
+}
